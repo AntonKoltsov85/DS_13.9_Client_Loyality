d8e4681054f2f493c334ebcc802f491753648b15
AntonKoltsov85 <94557326+AntonKoltsov85@users.noreply.github.com>
2023-02-04 19:13:40 +0300

    Delete Archive directory

--- Archive/movies.py
+++
@@ -1,6 +0,0 @@
-from IPython.display import display
-import pandas as pd
-from pathlib import Path
-
-ratings1=pd.read_csv('Users\user\Documents\Programming\IDE\data\movies_data\ratings1.csv', sep=',')
-display(ratings1.value_counts())
